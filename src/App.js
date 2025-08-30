73d6a5e3ef6365509ce79a95799b9374a44be5a4
Vamsi Koduri <vamsi.koduri@atforte.com>
2025-08-30 17:29:55 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {  Routes, Route, Navigate } from 'react-router-dom';
-import ManagerRoutes from './routes/ManagerRouter'; // make sure path and filename matches
+import ManagerRoutes from './routes/ManagerRouter'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Example from './screens/Example'; // Import the Example component
+import Example from './screens/Example'; 
 import Companies from './screens/Companies';
 import CompaniesInformation from './screens/CompaniesInformation';
 
