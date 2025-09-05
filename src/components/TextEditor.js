2bd2ccb4c9ad48227e07357e88a0547f67c1b03e
Vamsi Koduri <vamsi.koduri@atforte.com>
2025-09-05 12:27:30 +0530

    updated the text editor page

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,12 +1,10 @@
 // TextEditor.js
-import styles from './TextEditor.module.css'
+import styles from './TextEditor.module.css';
 import { Editor } from "@tinymce/tinymce-react";
 import { useRef } from 'react';
 
 const TextEditor = () => {
   const editorRef = useRef(null);
-
-
 
   const handleClear = () => {
     if (editorRef.current) {
@@ -16,31 +14,25 @@ const TextEditor = () => {
 
   return (
     <div className={`px-2 ${styles.editorContainer}`}>
-      <p className={`${styles.title}`}>Call history</p>
+      <p className={styles.title}>Call history</p>
       <div className={styles.pColor}>
         <Editor
-          apiKey="l42d054lu6hw84jq8nw1m8jc6flcn2svzz4r2qscoasx1nt1"
-          onInit={(evt, editor) => editorRef.current = editor}
+          tinymceScriptSrc="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.8.2/tinymce.min.js"
+          onInit={(evt, editor) => (editorRef.current = editor)}
           init={{
             height: 400,
             menubar: false,
-            plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table paste code help wordcount",
-            ],
-            toolbar:
-              "undo redo | formatselect | bold italic underline | copy pastetext",
+            plugins: ["lists", "link", "paste", "help", "wordcount", "checklist", "textcolor"],
+            toolbar: "bold italic underline | backcolor | checklist numlist bullist |copy paste",
             paste_as_text: true,
             body_class: styles.editorBody,
           }}
-         
         />
       </div>
-      <div className='d-flex justify-content-end gap-2 m-3'>
-        <button className='btn btn-primary'>cancel</button>
-        <button className='btn btn-primary'>save</button>
-        <button className='btn btn-primary' onClick={handleClear}>clear</button>
+      <div className="d-flex justify-content-end gap-2 m-3">
+        <button className="btn btn-primary">cancel</button>
+        <button className="btn btn-primary">save</button>
+        <button className="btn btn-primary" onClick={handleClear}>clear</button>
       </div>
     </div>
   );
